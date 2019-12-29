d5a1fb699353deb9c1514abee218745fbef88685
gwini <honzacimbal@gmail.com>
2019-12-29 14:39:48 +0100

    Create Yii2ValetDriver.php

---
+++ Yii2ValetDriver.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Class Yii2ValetDriver
+ */
+class Yii2ValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/vendor/yiisoft/yii2/Yii.php');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/web/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/web/index.php';
+        $_SERVER['SCRIPT_NAME'] = '/index.php';
+        $_SERVER['PHP_SELF'] = '/index.php';
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+
+        return $sitePath.'/web/index.php';
+    }
+}
